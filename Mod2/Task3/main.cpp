e1b9ee303f5653e8c6abe58cfc3df0996225e201
FackOff25 <kozvladimir@bk.ru>
2022-05-17 19:18:15 +0300

    BTree

--- Mod2/Task3/main.cpp
+++ Mod2/Task3/main.cpp
@@ -6,11 +6,11 @@ class IntComparator{
 	public:
 		IntComparator(){};
 		bool operator()(const int &first, const int &second){
-			return first > second;
+			return first < second;
 		};
 };
 
-template< typename T, class Comparator>
+template<typename T, class Comparator>
 class BTree{
 	private:
 
@@ -37,30 +37,55 @@ class BTree{
 		size_t t;
 		Comparator cmp;
 		bool isNodeFull(Node* node){
-			return node->keys() == 2*t - 1;
+			return node->keys.size() == 2*t - 1;
 		}
-		void splitChild(Node* node, size_t index);
-		void insertFull();
+		void splitChild(Node* node, size_t index){
+			Node* splitted = node->children[index];
+			Node* newNode = new Node(splitted->isLeaf);
+
+			for(int i = 0; i < t - 1; ++i)
+				newNode->keys.push_back(splitted->keys[i + t]);
+			
+			if(!splitted->isLeaf){
+				for(int i = 0; i < splitted->children.size() - 1; ++i)
+					newNode->children.push_back(splitted->children[i + t + 1]);
+			}
+
+			if (node->keys.size() != 0)
+				for(int i = node->keys.size() - 1; i > index; --i)
+					node->keys[i] = node->keys[i - 1];
+			else node->keys = std::vector<T>(1);
+
+			node->keys[index] = splitted->keys[t - 1];
+
+			for(int i = node->children.size() - 1; i > index + 1; --i)
+				node->children[i] = node->children[i - 1];
+			node->children[index] = newNode;
+
+			splitted->keys.resize(t - 1);
+		};
+		//void insertFull();
 		void insertNonFull(Node* node, const T& key){
 			int pos = node->keys.size() - 1;
+
 			if( node->isLeaf ){
 				node->keys.resize(node->keys.size() + 1);
-				while(pos >= 0 && cmp(key, node->children[ pos + 1 ]) ){
-					node->keys[pos + 1] = node.keys[pos];
+				while(pos >= 0 && cmp(key, node->keys[ pos + 1 ]) ){
+					node->keys[pos + 1] = node->keys[pos];
 					--pos;
 				}
 				node->keys[ pos + 1 ] = key;
 			}else{
-				while(pos >= 0 && node->children[ pos + 1 ] ){
+				while(pos >= 0 && cmp(key, node->keys[ pos + 1 ]) ){
 					--pos;
 				}
 				if(isNodeFull(node->children[ pos + 1 ])){
 					splitChild(node, pos + 1);
-					if( cmp(key, node->children[ pos + 1 ])){
+					if( cmp(key, node->keys[ pos + 1 ])){
 						++pos;
 					}
 				}
-
+				
 				insertNonFull(node->children[ pos + 1 ], key);
 			}
 		};
@@ -89,7 +114,8 @@ class BTree{
 			while(!layer.empty()){
 				for(Node* node: layer) {
 					node->print(out);
-					nextLayer.push_back(node->children);
+					for(Node* child: node->children)
+						nextLayer.push_back(child);
 				}
 				layer = nextLayer;
 				nextLayer = std::vector<Node*>();
@@ -104,6 +130,7 @@ int main(){
 
 	BTree<int, IntComparator> tree(num);
 
-	while( std::cin >> num ) tree.insert(num);
+	while( std::cin >> num ) 
+		tree.insert(num);
 	tree.printByLayers(std::cout);
 }
