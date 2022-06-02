1ecd9594c19cffc562a25f584b9f260daeacd5b7
FackOff25 <kozvladimir@bk.ru>
2022-06-02 21:50:24 +0300

    ListGraph implementation

--- Mod3/Task1/ListGraph.h
+++ Mod3/Task1/ListGraph.h
@@ -1,16 +1,25 @@
+#ifndef LISTGRAPH
+#define LISTGRAPH
+
 #include "iGraph.h"
 
 struct ListGraph : public IGraph {
-	ListGraph();
-	ListGraph(const IGraph& graph);
-	~ListGraph();
-	ListGraph operator=(const IGraph& graph);
-    
-    // Добавление ребра от from к to.
-	void AddEdge(int from, int to) = 0;
+	private:
+		std::vector<std::vector<int>> lists;
+		bool contains(std::vector<int> list, int vertex) const;
+	public:
+		ListGraph(int vertexNum = 0);
+		ListGraph(const IGraph& graph);
+		~ListGraph();
+		ListGraph operator=(const IGraph& graph);
+		
+		// Добавление ребра от from к to.
+		void AddEdge(int from, int to) override;
 
-    int VerticesCount() const;
+		int VerticesCount() const override;
 
-	std::vector<int> GetNextVertices(int vertex) const;
-	std::vector<int> GetPrevVertices(int vertex) const;
+		std::vector<int> GetNextVertices(int vertex) const override;
+		std::vector<int> GetPrevVertices(int vertex) const override;
 };
+
+#endif
